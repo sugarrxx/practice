ed84325ffb247349ab6ad0cb956601193414e74c
lixiaoxiang <lixiaoxiang@vpgame.cn>
2019-08-16 16:18:25 +0800

    Java Singleton Pattern

---
+++ src/main/java/com/example/study/designpattern/singleton/Singleton2.java
@@ -0,0 +1,28 @@
+package com.example.study.designpattern.singleton;
+
+/**
+ * 懒汉式
+ * 是否 Lazy 初始化：是
+ * <p>
+ * 是否多线程安全：是
+ * <p>
+ * 实现难度：易
+ * <p>
+ * 描述：这种方式具备很好的 lazy loading，能够在多线程中很好的工作，但是，效率很低，99% 情况下不需要同步。
+ *
+ * @author lixiaoxiang
+ */
+public class Singleton2 {
+
+    private static Singleton2 instance;
+
+    private Singleton2() {
+    }
+
+    public static synchronized Singleton2 getInstance() {
+        if (instance == null) {
+            instance = new Singleton2();
+        }
+        return instance;
+    }
+}
---
+++ src/main/java/com/example/study/designpattern/singleton/Singleton4.java
@@ -0,0 +1,35 @@
+package com.example.study.designpattern.singleton;
+
+/**
+ * 双检锁/双重校验锁（DCL，即 double-checked locking）
+ * JDK 版本：JDK1.5 起
+ * <p>
+ * 是否 Lazy 初始化：是
+ * <p>
+ * 是否多线程安全：是
+ * <p>
+ * 实现难度：较复杂
+ * <p>
+ * 描述：这种方式采用双锁机制，安全且在多线程情况下能保持高性能。
+ * getInstance() 的性能对应用程序很关键。
+ *
+ * @author lixiaoxiang
+ */
+public class Singleton4 {
+
+    private volatile static Singleton4 singleton;
+
+    private Singleton4() {
+    }
+
+    public static Singleton4 getSingleton() {
+        if (singleton == null) {
+            synchronized (Singleton4.class) {
+                if (singleton == null) {
+                    singleton = new Singleton4();
+                }
+            }
+        }
+        return singleton;
+    }
+}
